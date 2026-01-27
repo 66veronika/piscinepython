0abe4b63d8bfbf67f74678a88a6e61a88bfcf746
Veronika <vee66veen@gmail.com>
2026-01-27 20:29:31 +0100

    .

---
+++ module5/ex00/create_array.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+#array = [1, 3, 7, 20, 0, -5]
+#print("[", end="")
+#for number in array:
+#	if 
+#	print(", ", end="") 
+#	print(number, end="")
+#print("]", end="")
+
+array = [1, 3, 7, 20, 0, -5]
+print(array)
