20006195fef0b42e6ee13efd21af640b331487a2
Veronika <vee66veen@gmail.com>
2026-01-27 20:06:27 +0100

    .

---
+++ module4/ex04/round_up.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+import math
+
+number = float(input("Give me a number: "))
+number = math.ceil(number)
+print(number)
---
+++ module4/ex05/up_low.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+
+string = input()
+string = string.swapcase()
+print(string)
